9c2c15ceca0cf0998c4fb61985ee735cc2a5555f
MooZ <mooz@blockos.org>
2022-11-30 16:40:16 +0100

    Fixed OSX build.

--- scene_many_nodes.cpp
+++ scene_many_nodes.cpp
@@ -1,4 +1,6 @@
 // HARFANG(R) Copyright (C) 2021 Emmanuel Julien, NWNC HARFANG. Released under GPL/LGPL/Commercial Licence, see licence.txt for details.
+#include <cmath>
+
 #include <foundation/log.h>
 #include <foundation/clock.h>
 
